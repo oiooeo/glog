fc77d2aa5624f2447e6fbb653062faaa0bd425dc
jeon su jung <sj1910924@icloud.com>
2023-08-21 11:36:17 +0900

    feat. header&footer 라우터 연결 및 QueryClientProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { OverlayProvider } from './components/common/overlay/Overlay.context';
+import Router from './shared/Router';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <OverlayProvider>
+        <Router />
+      </OverlayProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
