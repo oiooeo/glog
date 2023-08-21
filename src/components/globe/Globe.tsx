f98ef6537f7b1ef1a4200db16528e10c8eefeeb5
seungbeom1999 <khhds00@naver.com>
2023-08-21 20:10:58 +0900

    feat. image 최대 6개까지 나머지 pin으로

--- src/components/globe/Globe.tsx
+++ src/components/globe/Globe.tsx
@@ -110,43 +110,77 @@ const Globe: React.FC<MapProps> = ({ initialCenter, zoom }) => {
         zoom: zoom,
       });
       // mapPost 기능때 사용
-      map.current.on('click', e => {
-        const coordinates = e.lngLat;
+      // map.current.on('click', e => {
+      //   const coordinates = e.lngLat;
 
-        map.current?.on('zoom', async () => {
-          const zoomSize = map.current?.getZoom();
-          //zoom이 9 이하일때는 삭제
-          console.log(zoomSize);
-          if (zoomSize && zoomSize <= 9) {
-            const oldMarker = document.querySelector('.new-marker');
-            if (oldMarker) {
-              oldMarker.remove();
-            }
-          }
-          // 확대가 다 됐을 경우 생성
-          if (zoomSize && zoomSize == 10) {
-            const oldMarker = document.querySelector('.new-marker');
-            if (oldMarker) {
-              oldMarker.remove();
-            }
-            const newMarker = document.createElement('div');
-            newMarker.className = 'new-marker';
-            newMarker.style.backgroundImage = `url(https://img.icons8.com/?size=512&id=21613&format=png)`;
-            newMarker.style.width = '30px';
-            newMarker.style.height = '30px';
-            newMarker.style.backgroundSize = '100%';
-            new mapboxgl.Marker(newMarker).setLngLat([coordinates.lng, coordinates.lat]).addTo(map.current!);
+      //   map.current?.on('zoom', async () => {
+      //     const zoomSize = map.current?.getZoom();
+      //     //zoom이 9 이하일때는 삭제
+      //     console.log(zoomSize);
+      //     if (zoomSize && zoomSize <= 9) {
+      //       const oldMarker = document.querySelector('.new-marker');
+      //       if (oldMarker) {
+      //         oldMarker.remove();
+      //       }
+      //     }
+      //     // 확대가 다 됐을 경우 생성
+      //     if (zoomSize && zoomSize == 10) {
+      //       const oldMarker = document.querySelector('.new-marker');
+      //       if (oldMarker) {
+      //         oldMarker.remove();
+      //       }
+      //       const newMarker = document.createElement('div');
+      //       newMarker.className = 'new-marker';
+      //       newMarker.style.backgroundImage = `url(https://img.icons8.com/?size=512&id=21613&format=png)`;
+      //       newMarker.style.width = '30px';
+      //       newMarker.style.height = '30px';
+      //       newMarker.style.backgroundSize = '100%';
+      //       new mapboxgl.Marker(newMarker).setLngLat([coordinates.lng, coordinates.lat]).addTo(map.current!);
 
-            placeLocation(coordinates);
-          }
+      //       placeLocation(coordinates);
+      //     }
+      //   });
+      //   // post 지역으로 확대
+      //   map.current?.flyTo({
+      //     center: [coordinates.lng, coordinates.lat],
+      //     zoom: 10,
+      //     speed: 0.8,
+      //   });
+      // });
+      for (let i = 0; i < geojson.features.length; i++) {
+        const marker = geojson.features[i];
+        // 작성한 marker 생성
+        const el = document.createElement('div');
+        el.className = 'marker';
+        if (i < 6) {
+          el.style.backgroundImage = `url(${marker.image}/40/40/)`;
+          el.style.width = `40px`;
+          el.style.height = `40px`;
+          el.style.backgroundSize = '100%';
+        } else {
+          el.style.backgroundImage = `url(https://img.icons8.com/?size=512&id=21613&format=png)`;
+          el.style.width = `20px`;
+          el.style.height = `20px`;
+          el.style.backgroundSize = '100%';
+        }
+        const markerInfo = {
+          lng: marker.coordinates.longitude,
+          lat: marker.coordinates.latitude,
+        };
+
+        el.addEventListener('click', async () => {
+          map.current?.flyTo({
+            center: [markerInfo.lng, markerInfo.lat],
+            zoom: 10, // 원하는 줌 레벨 설정
+            speed: 0.8, // 이동 속도 설정
+          });
+          map.current?.once('moveend', () => {
+            console.log('moveend');
+            placeLocation(markerInfo);
+          });
         });
-        // post 지역으로 확대
-        map.current?.flyTo({
-          center: [coordinates.lng, coordinates.lat],
-          zoom: 10,
-          speed: 0.8,
-        });
-      });
+        new mapboxgl.Marker(el).setLngLat([markerInfo.lng, markerInfo.lat]).addTo(map.current!);
+      }
     }
   }, [initialCenter, zoom]);
   return <div>Globe</div>;
