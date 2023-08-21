be192b00eb3537a33ec6ed9b0f0f82f2040ebf42
jeon su jung <sj1910924@icloud.com>
2023-08-21 15:42:39 +0900

    feat. 푸터 추가

--- src/components/common/footer/style.ts
+++ src/components/common/footer/style.ts
@@ -1,1 +1,19 @@
 import { styled } from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  background-color: transparent;
+  padding: 5px;
+  text-align: center;
+`;
+
+export const FooterText = styled.p`
+  margin: 0;
+  font-size: 12px;
+  color: #333;
+`;
+
+export const GitHubLink = styled.a`
+  color: #333;
+  font-size: 12px;
+  text-decoration: none;
+`;
