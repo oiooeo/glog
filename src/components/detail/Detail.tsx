83ae61f92e16343e88ae345eb156ff730b33f5c7
yena <jntantmsemt@naver.com>
2023-08-24 11:36:49 +0900

    design. 상세모달 말풍선 ui 및 post imgbox 글자 수정

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -11,15 +11,17 @@ const Detail = () => {
       <div>
         {data &&
           data.map(post => (
-            <Styled.ImageContainer key={post.id}>
-              <Styled.Image>
-                <Styled.ImageContent src={post.images!} alt={`Image for ${post.contents}`} />
-              </Styled.Image>
-              <h2>{post.contents}</h2>
-              <p>{post.countryId}</p>
-              <p>{post.regionId}</p>
-              <p>{post.createdAt}</p>
-            </Styled.ImageContainer>
+            <Styled.TalkBubble>
+              <Styled.ImageContainer key={post.id}>
+                <Styled.Image>
+                  <Styled.ImageContent src={post.images!} alt={`Image for ${post.contents}`} />
+                </Styled.Image>
+                <h2>{post.contents}</h2>
+                <p>{post.countryId}</p>
+                <p>{post.regionId}</p>
+                <p>{post.createdAt}</p>
+              </Styled.ImageContainer>
+            </Styled.TalkBubble>
           ))}
       </div>
     </Styled.Layout>
