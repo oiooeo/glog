5458692870390422549792e9a9470e04865e23e5
Choi Yoonseo <choiys1103@naver.com>
2023-08-25 20:10:17 +0900

    feat. 6개 이미지 마커 제외한 나머지 게시글 핀 마커 찍기

--- src/components/globe/style.ts
+++ src/components/globe/style.ts
@@ -4,6 +4,22 @@ export const GlobeLayout = styled.div`
   width: 100vw;
   height: 100vh;
 
+  & > .mapboxgl-canvas-container .image-marker {
+    border-radius: 10px;
+    background-size: cover;
+    z-index: 10;
+
+    &:hover {
+      outline: 3px greenyellow solid;
+    }
+  }
+
+  & > .mapboxgl-canvas-container .pin-marker {
+    width: 30px;
+    height: 100%;
+    background-size: cover;
+  }
+
   & > .mapboxgl-control-container .mapboxgl-ctrl-bottom-left {
     visibility: hidden;
   }
