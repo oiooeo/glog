042d32045390026254c3e6bacf8fb06d95dc19fa
seungbeom1999 <khhds00@naver.com>
2023-08-30 16:09:42 +0900

    refactor. GlobeCluster 파일 수정

--- src/components/globeSearch/GlobeSearch.tsx
+++ src/components/globeSearch/GlobeSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMapLocationStore } from '../../zustand/store';
 import { getSearchData } from '../../api/mapbox';
 import { useQuery } from '@tanstack/react-query';
